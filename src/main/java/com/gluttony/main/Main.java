a0758bd11c9f1962309e3b2c78f2c748ddc1f279
GluttonyLord <simon_camp@gmx.de>
2017-09-30 18:54:04 +0200

    initial commit

---
+++ src/main/java/com/gluttony/main/Main.java
@@ -0,0 +1,29 @@
+package com.gluttony.main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main extends Application {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(actionEvent -> logger.info("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+        stage.show();
+    }
+}
